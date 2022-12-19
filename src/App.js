17a2eea6b565dd29042d917302ea7e9897082110
imonthegrindset <imonthegrindset@gmail.com>
2022-12-19 15:45:47 -0500

    device rotation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar'
 import Home from './components/Home'
 import ValidatorProfile from './components/ValidatorProfile';
 import Proposals from './components/Proposals';
+import { BiRotateRight } from 'react-icons/bi'
 
 function App() {
   const [validators, setValidators] = useState([]);
@@ -16,7 +17,8 @@ function App() {
   const [currentProposal, setCurrentProposal] = useState([])
   const [stakedAtoms, setStakedAtoms] = useState(0);
   const [sentUpdate, setSentUpdate] = useState(false);
-
+  const [rotateMessage, setRotateMessage] = useState('Please Rotate Your Device')
+  const [device, setDevice] = useState("");
 
   const getCurrentValidator = (current) => {
     setCurrentValidator(current);
@@ -30,50 +32,65 @@ function App() {
     setSentUpdate(value);
   }
 
+
+
   useEffect(() => {
-    fetch('https://api.mintscan.io/v1/utils/params/chain/cosmos')
-    .then(res => {
-        return res.json()
-    })
-    .then(data => {
-        let bondedAtoms = data.params.staking_pool.pool.bonded_tokens;
-        bondedAtoms = Number(bondedAtoms.substr(0, bondedAtoms.length - 6));
+    // fetch('https://api.mintscan.io/v1/utils/params/chain/cosmos')
+    // .then(res => {
+    //     return res.json()
+    // })
+    // .then(data => {
+    //     let bondedAtoms = data.params.staking_pool.pool.bonded_tokens;
+    //     bondedAtoms = Number(bondedAtoms.substr(0, bondedAtoms.length - 6));
 
-        setStakedAtoms(bondedAtoms);
-    })
+    //     setStakedAtoms(bondedAtoms);
+    // })
 
-    fetch('https://api.mintscan.io/v1/cosmos/proposals', {
-      method: 'get'
-    })
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setProposals(data);
-      })
+    // fetch('https://api.mintscan.io/v1/cosmos/proposals', {
+    //   method: 'get'
+    // })
+    //   .then(res => {
+    //     return res.json()
+    //   })
+    //   .then(data => {
+    //     setProposals(data);
+    //   })
 
-    fetch('https://api.mintscan.io/v1/cosmos/validators', {
-      method: 'get'
-    })
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        let validatorArray = data;
+    // fetch('https://api.mintscan.io/v1/cosmos/validators', {
+    //   method: 'get'
+    // })
+    //   .then(res => {
+    //     return res.json()
+    //   })
+    //   .then(data => {
+    //     let validatorArray = data;
 
-        let filtered = validatorArray.filter(validator => validatorArray.indexOf(validator) < 175)
+    //     let filtered = validatorArray.filter(validator => validatorArray.indexOf(validator) < 175)
 
-        setValidators(filtered);
-      })
-      
+    //     setValidators(filtered);
+    //   })
+
+    function handleOrientationChange() {
+      console.log(window.screen.orientation.type);
+      console.log(window.navigator.userAgent)
+      if (window.screen.orientation.type === 'portrait-primary') {
+        setDevice("portrait")
+      } else {
+        setDevice("");
+      }
+    }
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+
   }, [])
-
-  
 
   useEffect(() => {
     sortPropsTurnout();
   }, [proposals])
-
 
   //SORT PROPOSALS BY TURNOUT
   function sortPropsTurnout() {
@@ -96,29 +113,35 @@ function App() {
     setTurnoutProposals(sortedTurnout)
   }
 
-  function promptRotate() {
-    if (window.matchMedia("(orientation: portrait)").matches) {
-      alert("Please rotate your device to landscape mode for a better viewing experience.");
-    }
-  }
-
   return (
-    <Router>
-      <div className='bg-gradient-to-br from-violet-200 to-white min-h-screen overflow-hidden'>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/validators' element={<ValidatorProfile 
-          sendUpdate={sendUpdate} sentUpdate={sentUpdate} 
-          validators={validators} proposals={proposals} currentValidator={currentValidator} getCurrentValidator={getCurrentValidator} getCurrentProposal={getCurrentProposal}
-          currentProposal={currentProposal}/>}></Route>
-          <Route path='/proposals' element={<Proposals 
-          sendUpdate={sendUpdate} 
-          stakedAtoms={stakedAtoms} validators={validators} proposals={proposals}  currentValidator={currentValidator} 
-          currentProposal={currentProposal} getCurrentProposal={getCurrentProposal} getCurrentValidator={getCurrentValidator} turnoutProposals={turnoutProposals} />}></Route>
-        </Routes>
-      </div>
 
+    <Router>
+      {device === "" ? (
+        <div className='bg-gradient-to-br from-violet-200 to-white min-h-screen overflow-hidden'>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/validators' element={<ValidatorProfile
+              sendUpdate={sendUpdate} sentUpdate={sentUpdate}
+              validators={validators} proposals={proposals} currentValidator={currentValidator} getCurrentValidator={getCurrentValidator} getCurrentProposal={getCurrentProposal}
+              currentProposal={currentProposal} />}></Route>
+            <Route path='/proposals' element={<Proposals
+              sendUpdate={sendUpdate}
+              stakedAtoms={stakedAtoms} validators={validators} proposals={proposals} currentValidator={currentValidator}
+              currentProposal={currentProposal} getCurrentProposal={getCurrentProposal} getCurrentValidator={getCurrentValidator} turnoutProposals={turnoutProposals} />}></Route>
+          </Routes>
+        </div>
+      ) : (
+        <><nav className="bg-indigo-800 flex flex-row gap-12 py-5 px-10 items-center justify-center">
+          <div className='text-2xl font-Titillium text-white self-center mb-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-blue-300'>GovernHUB
+          </div>
+        </nav>
+          <div className='bg-gradient-to-br from-violet-200 to-white min-h-screen overflow-hidden flex flex-col gap-5 items-center justify-center'>
+            <div className='font-bold text-3xl flex justify-center text-center text-indigo-700'>{rotateMessage}</div>
+            <div><BiRotateRight className='font-bold text-8xl text-indigo-700' /></div>
+          </div></>
+
+      )}
     </Router >
   );
 }
