01c85d6a8ada68c6fd7adf70b5ccdbed9e28bb05
imonthegrindset <imonthegrindset@gmail.com>
2022-12-19 21:26:01 -0500

    express server

--- server/server.js
+++ server/server.js
@@ -2,32 +2,61 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+let validators;
+let proposals;
+let atoms;
+
+
 app.use(
     cors({
         origin: 'http://localhost:3000'
     })
 )
 
+app.use(getValidators)
+app.use(getProposals)
+app.use(getAtoms)
 
-function getValidators() {
 
+async function getValidators(req, res, next) {
+    let response = await fetch('https://api.mintscan.io/v1/cosmos/validators');
+    validators = response.json()
+
+    next();
 }
 
-function getProposals() {
 
+async function getProposals(req, res, next) {
+    let response = await fetch('https://api.mintscan.io/v1/cosmos/proposals');
+    proposals = response.json()
+
+    next();
 }
 
-function getAtom() {
+async function getAtoms (req, res, next) {
+    let response = await fetch('https://api.mintscan.io/v1/utils/params/chain/cosmos');
+    atoms = response.json();
 
+    next();
 }
 
-function getSelectedValidatorVotes() {
-
-}
 
 
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+
+app.get('/validators', async (req, res) => {
+    res.json(await validators);
+})
+
+app.get('/proposals', async (req, res) => {
+    res.json(await proposals);
+})
+
+app.get('/atoms', async (req, res) => {
+    console.log(await atoms)
+    res.json(await atoms);
 })
 
 
