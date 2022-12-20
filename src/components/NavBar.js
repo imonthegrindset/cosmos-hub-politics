939ed8a6364ac4bcfc68a30e3ac2046e50aa09ee
imonthegrindset <imonthegrindset@gmail.com>
2022-12-19 21:35:44 -0500

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar(props) {
         <>
             <nav className="bg-indigo-800 flex flex-row gap-12 py-5 px-10 items-center">
                 <div className='text-2xl font-Titillium text-white self-center mb-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-blue-300'>GovernHUB</div>
-                <Link to='/cosmos-hub-politics/'>
+                <Link to='/'>
                     <div className='text-lg font-Titillium p-1 px-2 rounded-md text-white transition ease-out delay-50 bg-color hover:bg-indigo-500 cursor-pointer'>Home</div>
                 </Link>
 
